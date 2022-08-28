7bdc4a376ab507e6a31b2534f5a427ae8c814e28
ivanclay <ivan.clay.moura@gmail.com>
2022-08-28 13:16:51 -0300

    feat: create entities

---
+++ BookingService/Core/Domain/DomainEntities/Room.cs
@@ -0,0 +1,27 @@
+﻿namespace Domain.DomainEntities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public bool InMaintenance { get; set; }
+        public bool HasGuest { 
+            //verify open bookings to this room
+            get { return true; } 
+        }
+        public bool IsAvailable
+        {
+            get 
+            { 
+                if(this.InMaintenance || this.HasGuest)
+                {
+                    return false;
+                }
+                return true;
+            }
+            
+        }
+
+    }
+}
---
+++ BookingService/Core/Domain/Ports/IGuestRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.DomainEntities;
+
+namespace Domain.Ports
+{
+    public interface IGuestRepository
+    {
+        Task<Guest> Get(int Id);
+        Task<int> Save(int Id);
+    }
+}
